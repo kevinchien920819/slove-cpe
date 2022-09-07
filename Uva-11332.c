33c436a9c7776389b7104bdbb1b19cd4f1dd1d01
icebird <22712286+kevinchien920819@users.noreply.github.com>
2022-09-07 10:27:42 +0800

    Update Uva-11332.c
    processing update

--- Uva-11332.c
+++ Uva-11332.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int counting(int n){
-    int count = 0;
+int counting(unsigned long long int n,int count){
+    
     count = n % 10 + count;
     n = n / 10;
-    if(n == 0 && count / 10 ==0) return count;
-    return counting(n);
+    if(n == 0 ) 
+		return count;
+    return counting(n,count);
 }
 int main (){
-    int n ;
-    while(scanf("%d",&n)){
+    unsigned long long int n ;
+    while(scanf("%llu",&n)){
         if(n == 0)
             return 0;
-        else        
-            printf("%d\n",counting(n));
+        else
+            printf("%d\n",counting(n,0));
     }
 }
